073f4407728ff5ea59db969c8bb02728ad991dbe
Zach Sanders <zachsanders@Mac.focustapps.com>
2025-09-04 10:30:24 -0400

    changing utils to have an override

--- common/utils.py
+++ common/utils.py
@@ -6,17 +6,24 @@ from pathlib import Path
 from urllib import response
 from datetime import datetime
 
+# Fenrir becomes /s/ in CI
+PARENT_DIR_NAMES = ["Fenrir", "fenrir", "s", "FTS", "fts"]
 
-def get_project_root() -> Path:
+
+def get_project_root(target_override: str | None = None) -> Path:
     """
     Returns the Path to the Fenrir project root directory.
     """
     current_path = Path(__file__).resolve()
+    if target_override:
+        for parent in current_path.parents:
+            if parent.name == target_override:
+                return parent
+
     for parent in current_path.parents:
-        # Fenrir becomes /s/ in CI
-        if parent.name == "Fenrir" or parent.name == "fenrir" or parent.name == "s":
+        if parent.name in PARENT_DIR_NAMES:
             return parent
-    raise RuntimeError("Fenrir project root directory not found.")
+    raise RuntimeError(f"Fenrir project root directory not found. \n {current_path}")
 
 
 def get_subdirectories(
