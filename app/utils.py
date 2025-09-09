2de3e56dcc6c13b463a0007f98af58d8020221a1
Zach Sanders <zachsanders@Mac.lan>
2025-09-09 09:21:29 -0400

    changing optional arg to explicit type definition

--- app/utils.py
+++ app/utils.py
@@ -3,13 +3,18 @@ from pathlib import Path
 
 PARENT_DIR_NAMES = ["Fenrir", "fenrir", "s", "FTS", "fts"]
 
-def get_project_root() -> Path:
+
+def get_project_root(target_override: str | None = None) -> Path:
     """
     Returns the Path to the Fenrir project root directory.
     """
     current_path = Path(__file__).resolve()
+    if target_override:
+        for parent in current_path.parents:
+            if parent.name == target_override:
+                return parent
+
     for parent in current_path.parents:
-        # Fenrir becomes /s/ in CI
         if parent.name in PARENT_DIR_NAMES:
             return parent
-    raise RuntimeError("Fenrir project root directory not found.")
+    raise RuntimeError(f"Fenrir project root directory not found. \n {current_path}")
